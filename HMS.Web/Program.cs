ff83dd9a68b43769a958058105e2e79d38baee14
marshalnanda <mknanda678@gmail.com>
2023-09-17 00:06:25 +0530

    Db Roles

--- HMS.Web/Program.cs
+++ HMS.Web/Program.cs
@@ -1,8 +1,16 @@
+using Hospital.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Hospital;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
